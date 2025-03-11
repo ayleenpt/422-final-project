e40604a51c540b6bc3cc68430b3a67bcb37d7be3
NainooG <khush.nainn@gmail.com>
2025-03-10 17:03:32 -0700

    Update timer_template.s
    redid _timer_update

--- Thumb2_Programs/timer_template.s
+++ Thumb2_Programs/timer_template.s
@@ -32,9 +32,9 @@ _timer_init
 			LDR     R0, =STRELOAD_MX  ; Load max value
 			STR     R0, [R1]
 
-			LDR     R1, =STCURRENT
-			MOV     R0, #STCURR_CLR   ; Clear counter
-			STR     R0, [R1]
+;			LDR     R1, =STCURRENT
+;			MOV     R0, #STCURR_CLR   ; Clear counter
+;			STR     R0, [R1]
 			
 			LDMFD	SP!, {R4-R12, LR}
 			BX		LR		; return to Reset_Handler
@@ -43,7 +43,7 @@ _timer_init
 ; Timer start
 ; int timer_start( int seconds )
 		EXPORT		_timer_start
-_timer_start
+_timer_start ;;;;;;;;;;;; work on this 
 	; Implement by yourself (from claude)
 			STMFD   SP!, {R4-R12, LR}
     
@@ -74,35 +74,69 @@ _timer_start
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Timer update
 ; void timer_update( )
+
+; System Variables
+
+; System Timer Definition
+;STCTRL		EQU		0xE000E010		; SysTick Control and Status Register
+;STRELOAD	EQU		0xE000E014		; SysTick Reload Value Register
+;STCURRENT	EQU		0xE000E018		; SysTick Current Value Register
+;	
+;STCTRL_STOP	EQU		0x00000004		; Bit 2 (CLK_SRC) = 1, Bit 1 (INT_EN) = 0, Bit 0 (ENABLE) = 0
+;STCTRL_GO	EQU		0x00000007		; Bit 2 (CLK_SRC) = 1, Bit 1 (INT_EN) = 1, Bit 0 (ENABLE) = 1
+;STRELOAD_MX	EQU		0x00FFFFFF		; MAX Value = 1/16MHz * 16M = 1 second
+;STCURR_CLR	EQU		0x00000000		; Clear STCURRENT and STCTRL.COUNT	
+;SIGALRM		EQU		14				; sig alarm
+
+;; System Variables
+;SECOND_LEFT		EQU		0x20007B80		; Secounds left for alarm( )
+;USR_HANDLER     EQU		0x20007B84		; Address of a user-given signal handler function	
+	
 		EXPORT		_timer_update
 _timer_update
-	;; Implement by yourself
+		;; Implement by yourself
+		
 		STMFD   SP!, {R4-R12, LR}
+		
+		; read value at SECOND_LEFT
+		LDR		R0, =SECOND_LEFT
+		
+		; decrement
+		SUB		R0, R0, #1
+		
+		; if value hasn't reached 0, branch to timer update done
+		CMP		R0, #0
+		BNE		_timer_update_done
+		
+		; value has reached 0, stop timer
+		BEQ		_timer_stop
 
-		LDR     R1, =SECOND_LEFT
-		LDR     R0, [R1]        ; Load current seconds left
-		SUBS    R0, R0, #1      ; Decrement counter
-		STR     R0, [R1]        ; Store updated counter
-
-		BNE     _timer_update_done  ; If not zero, return
-
-		; If counter reached zero, stop timer
-		LDR     R1, =STCTRL
-		MOV     R0, #STCTRL_STOP
-		STR     R0, [R1]
-
-		; Load user-defined handler from USR_HANDLER
-		LDR     R1, =USR_HANDLER
-		LDR     R1, [R1]
-		CMP     R1, #0
-		BEQ     _timer_update_done  ; Skip if no handler is set
-
-		BLX     R1                 ; Call the user-defined handler
-
-_timer_update_done
+_timer_update_done ; return to wherever it was called here
 		POP     {R4, LR}
 		LDMFD	SP!, {R4-R12, LR}
 		BX		LR		; return to SysTick_Handler
+
+_timer_stop
+		; load user function into R1
+		LDR		R1, =USR_HANDLER
+		
+		; stop timer by writing STCTRL_STOP to STCTRL register
+		LDR		R2, =STCTRL_STOP
+		LDR		R3, =STCTRL
+		STR		R2, [R3]
+		
+		; branch and link
+		; stores where program is currently at in LR, then it calls function. so when we return to the callee, we know where to go.
+		BLX		R1
+
+
+;		; Load user-defined handler from USR_HANDLER
+;		LDR     R1, =USR_HANDLER
+;		LDR     R1, [R1]
+;		CMP     R1, #0
+;		BEQ     _timer_update_done  ; Skip if no handler is set
+
+;		BLX     R1                 ; Call the user-defined handler
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Timer update
